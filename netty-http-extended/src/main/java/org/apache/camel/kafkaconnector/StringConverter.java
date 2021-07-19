c3b51bedf8abb378d7769013e38aacecdb2a4e63
Emelyanov, Vadim [External] <Vadim.Emelyanov@externals.adidas.com>
2021-07-19 18:40:26 +0300

    Source connect properties updated, MDC, baggage item

--- netty-http-extended/src/main/java/org/apache/camel/kafkaconnector/StringConverter.java
+++ netty-http-extended/src/main/java/org/apache/camel/kafkaconnector/StringConverter.java
@@ -1,9 +1,5 @@
 package org.apache.camel.kafkaconnector;
 
-import com.google.common.collect.ImmutableMap;
-import io.jaegertracing.Configuration;
-import io.jaegertracing.internal.JaegerTracer;
-import io.opentracing.Span;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -15,21 +11,16 @@ import org.apache.kafka.connect.storage.Converter;
 import org.apache.kafka.connect.storage.ConverterType;
 import org.apache.kafka.connect.storage.HeaderConverter;
 import org.apache.kafka.connect.storage.StringConverterConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class CustomMessageConverter implements Converter, HeaderConverter {
+public class StringConverter implements Converter, HeaderConverter {
 
     private final StringSerializer serializer = new StringSerializer();
     private final StringDeserializer deserializer = new StringDeserializer();
 
-    private static final Logger LOG = LoggerFactory.getLogger(CustomMessageConverter.class);
-
-    public CustomMessageConverter() {
+    public StringConverter() {
     }
 
     @Override
@@ -70,35 +61,8 @@ public class CustomMessageConverter implements Converter, HeaderConverter {
 
     @Override
     public SchemaAndValue toConnectData(String topic, byte[] value) {
-        LOG.info("+++ Input message byte array length: " + value.length);
         try {
-            SchemaAndValue schemaAndValue = new SchemaAndValue(Schema.OPTIONAL_STRING_SCHEMA, deserializer.deserialize(topic, value));
-            LOG.info("+++ Deserialized message length: " + schemaAndValue.value().toString().length());
-            LOG.info("+++ Deserialized message schema name: " + schemaAndValue.schema().name());
-            LOG.info("+++ Deserialized message schema type: " + schemaAndValue.schema().type().getName());
-            LOG.info("+++ Is deserialized message schema optional: " + schemaAndValue.schema().isOptional());
-
-            //Jaeger tracing
-            Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
-            Configuration.SenderConfiguration senderConfig = Configuration.SenderConfiguration.fromEnv().withAgentHost("host.docker.internal");
-            Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true).withSender(senderConfig);
-            Configuration config = new Configuration("adidas-poc").withSampler(samplerConfig).withReporter(reporterConfig);
-            JaegerTracer tracer = config.getTracer();
-
-            MDC.put("tracer-metrics", tracer.getMetrics().toString());
-            LOG.info("+++ tracer-metrics: " + MDC.get("tracer-metrics"));
-            MDC.remove("tracer-metrics");
-
-            Span span = tracer.buildSpan("adidas-poc-span").start();
-            span.setTag("adidas-poc-tag", "Some tag value");
-            span.setBaggageItem("adidas-poc-baggage-item", "baggage-item-value");
-            span.log(ImmutableMap.of("Adidas PoC log", "This is the log sample",
-                    "Custom field", "And this is just custom field value",
-                    "message", schemaAndValue.value().toString()));
-            span.finish();
-
-            return  schemaAndValue;
-            //return new SchemaAndValue(Schema.OPTIONAL_STRING_SCHEMA, deserializer.deserialize(topic, value));
+            return new SchemaAndValue(Schema.OPTIONAL_STRING_SCHEMA, deserializer.deserialize(topic, value));
         } catch (SerializationException e) {
             throw new DataException("Failed to deserialize string: ", e);
         }
