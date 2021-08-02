59ebef4c18788c4f33a60eb0067fb7553aa50706
Emelyanov, Vadim [External] <Vadim.Emelyanov@externals.adidas.com>
2021-08-02 19:40:28 +0300

    Spring Boot Camel Sink Connector

---
+++ spring-boot-camel-sink/src/main/java/com/adikafka/poc/springbootcamelsink/SpringBootCamelSinkApplication.java
@@ -0,0 +1,13 @@
+package com.adikafka.poc.springbootcamelsink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamelSinkApplication.class, args);
+	}
+
+}
---
+++ spring-boot-camel-sink/src/test/java/com/adikafka/poc/springbootcamelsink/SpringBootCamelSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adikafka.poc.springbootcamelsink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCamelSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
