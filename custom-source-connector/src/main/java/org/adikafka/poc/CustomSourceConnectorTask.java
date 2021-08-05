715a0aa2f4d6b3ef01164eacf78ae95308917b87
Emelyanov, Vadim [External] <Vadim.Emelyanov@externals.adidas.com>
2021-08-05 18:09:05 +0300

    Custom Source connector: Jaeger, Holmes

--- custom-source-connector/src/main/java/org/adikafka/poc/CustomSourceConnectorTask.java
+++ custom-source-connector/src/main/java/org/adikafka/poc/CustomSourceConnectorTask.java
@@ -5,9 +5,15 @@ import io.jaegertracing.Configuration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.opentracing.Scope;
 import io.opentracing.Span;
+import io.opentracing.tag.Tags;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.adikafka.poc.holmes.logger.HolmesMessage;
+import org.adikafka.poc.holmes.model.AuditType;
+import org.adikafka.poc.holmes.model.Level;
+import org.adikafka.poc.holmes.model.impl.*;
+import org.adikafka.poc.holmes.util.LoggingUtils;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
@@ -19,10 +25,14 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.*;
 
+import static org.adikafka.poc.holmes.config.LoggingConfigProvider.getLoggingConfig;
+import static org.adikafka.poc.holmes.constant.LoggingConstants.LOGGING_KAFKA_VERSION;
+
 public class CustomSourceConnectorTask extends SourceTask {
 
     private static final Logger log = LoggerFactory.getLogger(CustomSourceConnectorTask.class);
     public CustomSourceConnectorConfig config;
+    private JaegerTracer tracer;
 
     @Override
     public String version() {
@@ -32,11 +42,20 @@ public class CustomSourceConnectorTask extends SourceTask {
     @Override
     public void start(Map<String, String> props) {
         config = new CustomSourceConnectorConfig(props);
+
+        //Tracer configuration
+        Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
+        Configuration.SenderConfiguration senderConfig = Configuration.SenderConfiguration.fromEnv().withAgentHost("host.docker.internal");
+        Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true).withSender(senderConfig);
+        Configuration config = new Configuration("poc-source-connector-service").withSampler(samplerConfig).withReporter(reporterConfig);
+        this.tracer = config.getTracer();
     }
 
     @Override
     public List<SourceRecord> poll() throws InterruptedException {
         log.info("+++ POLL METHOD STARTS");
+        Span span = tracer.buildSpan("poll").start();
+        span.setTag("step", "pollMethodStart");
 
         List<SourceRecord> records = new ArrayList<>();
         OkHttpClient client = new OkHttpClient();
@@ -46,21 +65,22 @@ public class CustomSourceConnectorTask extends SourceTask {
                 .get()
                 .build();
 
-        try {
+        try(Scope scope = tracer.scopeManager().activate(span)) {
             log.info("+++ EXECUTE REQUEST");
             Response response = client.newCall(request).execute();
-            Schema schema = SchemaBuilder.struct()
-                    .name("ItemValue")
-                    .parameters(Collections.emptyMap())
-                    .field("value", Schema.STRING_SCHEMA)
-                    .build();
-            Struct struct = new Struct(schema).put("value", Objects.requireNonNull(response.body()).toString());
-            SourceRecord record = new SourceRecord(Collections.emptyMap(), Collections.emptyMap(), "output", schema, struct);
-            records.add(record);
+            if (response.isSuccessful()) {
+                successfulResponseSpan(response.code());
+            } else {
+                unsuccessfulResponseSpan(response.code());
+            }
+            records.add(buildRecord(response));
+            toKafkaSpan();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            logHolmes();
+            span.finish();
         }
-
         return records;
     }
 
@@ -69,7 +89,73 @@ public class CustomSourceConnectorTask extends SourceTask {
         log.info("{} Stopping RestSourceTask.", this);
     }
 
-    private void pushTrace() {
+    private SourceRecord buildRecord(Response response) {
+        Schema schema = SchemaBuilder.struct()
+                .name("ItemValue")
+                .parameters(Collections.emptyMap())
+                .field("value", Schema.STRING_SCHEMA)
+                .build();
+        Struct struct = new Struct(schema).put("value", Objects.requireNonNull(response.body()).toString());
+        return new SourceRecord(Collections.emptyMap(), Collections.emptyMap(), "output", schema, struct);
+    }
+
+    private void logHolmes() {
+        //Holmes log
+        HolmesLog holmesLog = HolmesLog.newBuilder()
+                .message("Kafka Source Connector: Sample Holmes log message")
+                .timestampNow()
+                .labels(Label.DEV)
+                .service(Service.newBuilder()
+                        .ephemeralId(getLoggingConfig().getPropertyWithDefaultUnknown("ephemeralId"))
+                        .version(getLoggingConfig().getPropertyWithDefaultUnknown(LOGGING_KAFKA_VERSION))
+                        .build()
+                )
+                .log(Log.newBuilder()
+                        .level(Level.INFO)
+                        .logMessage(AuditMessage.newBuilder()
+                                .timestampNow()
+                                .corrID("corrID-value")
+                                .bussObjID("bussObjID-value")
+                                .logAttribute("LOG_ATTRIBUTE", "log attribute value")
+                                .additionalMsg("additional message")
+                                .auditType(AuditType.END).build()
+                        )
+                        .initializeMessageContext(LoggingUtils.INITIALIZE_MESSAGE_CONTEXT_OBJECT).build()
+                ).build();
+        log.info("+++ HOLMES message: " + new HolmesMessage(holmesLog).getFormattedMessage());
+    }
+
+    private void successfulResponseSpan(int code) {
+        Span span = tracer.buildSpan("successfulResponse").start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
+            span.log(ImmutableMap.of("responseCode", String.valueOf(code)));
+            span.setTag("step", "responseReceived");
+        } finally {
+            span.finish();
+        }
+    }
+
+    private void unsuccessfulResponseSpan(int code) {
+        Span span = tracer.buildSpan("successfulResponse").start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
+            span.setTag(Tags.ERROR, true);
+            span.log(ImmutableMap.of("responseCode", String.valueOf(code)));
+            span.setTag("step", "responseReceived");
+        } finally {
+            span.finish();
+        }
+    }
+
+    private void toKafkaSpan() {
+        Span span = tracer.buildSpan("toKafka").start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
+            span.setTag("step", "toKafka");
+        } finally {
+            span.finish();
+        }
+    }
+
+    /*private void pushTrace() {
         //Tracer configuration
         Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
         Configuration.SenderConfiguration senderConfig = Configuration.SenderConfiguration.fromEnv().withAgentHost("localhost");
@@ -111,5 +197,5 @@ public class CustomSourceConnectorTask extends SourceTask {
         } finally {
             span.finish();
         }
-    }
+    }*/
 }
